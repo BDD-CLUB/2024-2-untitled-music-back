3617a146d148c41c4008cacaf088cb26f8913f31
lcqff <yeonnu82g@gmail.com>
2024-09-08 14:21:42 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/MusicPlatform/MusicPlatformApplication.java
@@ -0,0 +1,13 @@
+package MusicPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MusicPlatform/MusicPlatform/MusicPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package MusicPlatform.MusicPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
