35a9f704ac2724ec5e2794f0c539c086ed235496
lcqff <yeonnu82g@gmail.com>
2024-09-26 13:49:13 +0900

    feat: Profile 엔티티 생성
    #9

---
+++ src/main/java/MusicPlatform/domain/profile/Profile.java
@@ -0,0 +1,39 @@
+package MusicPlatform.domain.profile;
+
+import MusicPlatform.domain.artist.entity.Artist;
+import MusicPlatform.global.entity.UuidEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Table(name = "profile")
+@SQLRestriction("is_deleted = false")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE profile SET is_deleted = true where id = ?")
+public class Profile extends UuidEntity {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String profileImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ARTIST_ID", nullable = false)
+    private Artist artist;
+
+    @Column(nullable = false)
+    private boolean isMain; // true: Artist 정보와 동일한 프로필 (메인 프로필)
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+}
