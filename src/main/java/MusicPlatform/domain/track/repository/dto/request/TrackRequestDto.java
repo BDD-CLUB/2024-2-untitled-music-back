a3176b4cc049fb5cd5c42cc1a4a3d7afd3053f7d
lcqff <yeonnu82g@gmail.com>
2024-10-03 17:45:29 +0900

    feat: Track api에 대한 Swagger 문서 작성 및 트랙 업로드 파라미터 처리방식 변경 - Swagger는 @RequestPart에 대한 처리가 명확하지 못함 - @ReqeustPart를 @ModelAttribute로 변경 및 dto에 MultipartFile 추가

--- src/main/java/MusicPlatform/domain/track/repository/dto/request/TrackRequestDto.java
+++ src/main/java/MusicPlatform/domain/track/repository/dto/request/TrackRequestDto.java
@@ -2,12 +2,15 @@ package MusicPlatform.domain.track.repository.dto.request;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
 
 public record TrackRequestDto(
         @NotBlank
         String title,
 
         @Nullable
-        String lyric
+        String lyric,
+
+         MultipartFile TrackFile
 ) {
 }
