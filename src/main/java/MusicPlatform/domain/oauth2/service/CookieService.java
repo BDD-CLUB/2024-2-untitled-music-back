6637f2643abad4f6addb69f9b526f4f6557a7ff6
YeonHu Yim <71930280+lcqff@users.noreply.github.com>
2024-10-31 18:19:41 +0900

    chore: @Slf4j 의존성 추가

--- src/main/java/MusicPlatform/domain/oauth2/service/CookieService.java
+++ src/main/java/MusicPlatform/domain/oauth2/service/CookieService.java
@@ -6,7 +6,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CookieService {
