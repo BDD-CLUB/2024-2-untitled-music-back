2dba4dcee281ba9d78880759e33ade8329ce8266
lcqff <yeonnu82g@gmail.com>
2024-11-08 19:30:01 +0900

    feat: clean.sql 작성
    #28

---
+++ src/test/resources/clean.sql
@@ -0,0 +1,23 @@
+delete
+from album;
+
+delete
+from album_like;
+
+delete
+from album_comment;
+
+delete
+from profile;
+
+delete
+from artist;
+
+delete
+from playlist_item;
+
+delete
+from playlist;
+
+delete
+from track;
