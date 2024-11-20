1cbe8d63388147ac47a47cf49bea192f4422f502
lcqff <yeonnu82g@gmail.com>
2024-11-20 17:31:26 +0900

    remove: 불필요한 import문 삭제

--- src/main/java/MusicPlatform/global/handler/OauthAccessDeniedHandler.java
+++ src/main/java/MusicPlatform/global/handler/OauthAccessDeniedHandler.java
@@ -1,16 +1,9 @@
 package MusicPlatform.global.handler;
 
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
