1983acd8b0222e9e15932d851fcec15542c9fc45
YeonHu Yim <71930280+lcqff@users.noreply.github.com>
2024-12-13 09:20:17 +0900

    fix: 쉼표를 추가한다.

--- src/main/java/MusicPlatform/global/error/BusinessError.java
+++ src/main/java/MusicPlatform/global/error/BusinessError.java
@@ -18,7 +18,7 @@ public enum BusinessError {
 
     //Playlist
     NOT_FOUND_PLAYLIST(HttpStatus.NOT_FOUND, "플레이리스트를 찾을 수 없습니다."),
-    FORBIDDEN_PLAYLIST_ACCESS(HttpStatus.FORBIDDEN, "플레이리스트 접근 권한이 없습니다.")
+    FORBIDDEN_PLAYLIST_ACCESS(HttpStatus.FORBIDDEN, "플레이리스트 접근 권한이 없습니다."),
 
     //Profile
     NOT_FOUND_PROFILE(HttpStatus.NOT_FOUND, "프로필을 찾을 수 없습니다."),
