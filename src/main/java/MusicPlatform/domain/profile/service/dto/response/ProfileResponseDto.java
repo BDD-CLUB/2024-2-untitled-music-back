9c77e7ebfd443dbb9ad0c205dcc7978c56b7037b
lcqff <yeonnu82g@gmail.com>
2024-12-23 16:44:27 +0900

    fix: ProfileResponseDto가 uuid를 반환하도록 수정한다.
    #53

--- src/main/java/MusicPlatform/domain/profile/service/dto/response/ProfileResponseDto.java
+++ src/main/java/MusicPlatform/domain/profile/service/dto/response/ProfileResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 
 @Builder
 public record ProfileResponseDto(
+        String uuid,
         String name,
         String description,
         String link1,
@@ -14,6 +15,7 @@ public record ProfileResponseDto(
 ) {
     public static ProfileResponseDto from(Profile profile) {
         return ProfileResponseDto.builder()
+                .uuid(profile.getUuid())
                 .name(profile.getName())
                 .description(profile.getDescription())
                 .link1(profile.getLink1())
