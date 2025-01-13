5d5198dd1565f39835050f6665b015cfc1d7bd28
lcqff <yeonnu82g@gmail.com>
2025-01-13 15:32:16 +0900

    refactor: 불필요한 코드를 삭제한다.
    #74

--- src/main/java/MusicPlatform/domain/oauth2/entity/OAuth2ProviderUser.java
+++ src/main/java/MusicPlatform/domain/oauth2/entity/OAuth2ProviderUser.java
@@ -1,10 +1,8 @@
 package MusicPlatform.domain.oauth2.entity;
 
 import MusicPlatform.domain.artist.entity.Role;
-import MusicPlatform.domain.oauth2.adapter.AuthenticationAdapter;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -13,7 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 /**
  * 모든 서비스 제공자가 동일하게 제공하는 속성들
  */
-public abstract class OAuth2ProviderUser implements ProviderUser, AuthenticationAdapter {
+public abstract class OAuth2ProviderUser implements ProviderUser {
 
     private String uuid;
     private Role role;
@@ -27,11 +25,6 @@ public abstract class OAuth2ProviderUser implements ProviderUser, Authentication
         this.attributes = attributes;
         this.oAuth2User = oAuth2User;
         this.clientRegistration = clientRegistration;
-    }
-
-    @Override
-    public String getPassword() {
-        return UUID.randomUUID().toString();
     }
 
     @Override
@@ -62,16 +55,6 @@ public abstract class OAuth2ProviderUser implements ProviderUser, Authentication
     @Override
     public String getUuid() {
         return this.uuid;
-    }
-
-    @Override
-    public String toString() {
-        return this.uuid;
-    }
-
-    @Override
-    public Role getRole() {
-        return this.role;
     }
 
     @Override
