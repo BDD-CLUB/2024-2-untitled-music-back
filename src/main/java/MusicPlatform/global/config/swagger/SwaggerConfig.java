ac7077c81c9f92834d5f8a17213fb081193143bf
lcqff <yeonnu82g@gmail.com>
2025-01-18 19:04:46 +0900

    feat: Swagger 사용시 JWT 토큰 사용 가능하도록 수정

--- src/main/java/MusicPlatform/global/config/swagger/SwaggerConfig.java
+++ src/main/java/MusicPlatform/global/config/swagger/SwaggerConfig.java
@@ -3,6 +3,8 @@ package MusicPlatform.global.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -22,11 +24,20 @@ public class SwaggerConfig {
                 .title("Untitled API Document")
                 .version("v0.1.0-alpha");
 
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
         Server server = new Server();
         server.setUrl(api);
 
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
                 .info(info)
                 .servers(List.of(server));
     }
