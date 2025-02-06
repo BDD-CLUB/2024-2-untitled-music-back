1e00dd071742e061bcb4f976ea3897d912517bfc
lcqff <yeonnu82g@gmail.com>
2025-02-06 19:20:34 +0900

    feat: 삭제된 Track을 조회하는 경우 PlaylistItemDto의 isDisabled 필드를 설정한다. #91

--- src/main/java/MusicPlatform/domain/playlist/_item/service/dto/response/PlaylistItemResponseDto.java
+++ src/main/java/MusicPlatform/domain/playlist/_item/service/dto/response/PlaylistItemResponseDto.java
@@ -7,11 +7,14 @@ import lombok.Builder;
 @Builder
 public record PlaylistItemResponseDto(
         String uuid,
-       TrackFullResponseDto trackGetResponseDto
+        Boolean isDisabled,
+        TrackFullResponseDto trackGetResponseDto
 ) {
     public static PlaylistItemResponseDto from(PlaylistItem playlistItem) {
+        // Track의 상태가 변경됨에 따라 연관된 모든 Playlist를 업데이트하기 보다는 Playlist 조회 시 필터링하도록 한다.
         return PlaylistItemResponseDto.builder()
                 .uuid(playlistItem.getUuid())
+                .isDisabled(playlistItem.getTrack().isDeleted())
                 .trackGetResponseDto(TrackFullResponseDto.from(playlistItem.getTrack()))
                 .build();
     }
