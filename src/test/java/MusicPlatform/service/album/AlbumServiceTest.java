6df08c111cc1667d32820c9301d7e3312b280cc3
lcqff <yeonnu82g@gmail.com>
2025-03-27 18:00:19 +0900

    rename: 파일 이름 변경

--- src/test/java/MusicPlatform/service/album/AlbumServiceTest.java
+++ src/test/java/MusicPlatform/service/album/AlbumServiceTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
 @Transactional
-@Sql("/sql/service-test-data.sql")
+@Sql("/sql/album-test-data.sql")
 public class AlbumServiceTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
