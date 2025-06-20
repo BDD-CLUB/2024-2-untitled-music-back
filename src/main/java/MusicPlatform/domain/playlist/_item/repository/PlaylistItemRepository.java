7660be470681d4fe85685aa87b0032faf92a4c6f
lcqff <yeonnu82g@gmail.com>
2025-06-20 21:28:25 +0900

    refactor: Page -> Slice 변경
    #102

--- src/main/java/MusicPlatform/domain/playlist/_item/repository/PlaylistItemRepository.java
+++ src/main/java/MusicPlatform/domain/playlist/_item/repository/PlaylistItemRepository.java
@@ -2,9 +2,8 @@ package MusicPlatform.domain.playlist._item.repository;
 
 import MusicPlatform.domain.playlist._item.entity.PlaylistItem;
 import MusicPlatform.domain.playlist.entity.Playlist;
-import java.util.List;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,6 +13,6 @@ public interface PlaylistItemRepository extends JpaRepository<PlaylistItem,Long>
             + "JOIN FETCH t.album a "
             + "JOIN FETCH a.artist at "
             + "WHERE p.playlist = :playlist")
-    Page<PlaylistItem> findAllByPlaylist(Playlist playlist, Pageable pageable);
+    Slice<PlaylistItem> findAllByPlaylist(Playlist playlist, Pageable pageable);
     void deleteByUuid(String uuid);
 }
