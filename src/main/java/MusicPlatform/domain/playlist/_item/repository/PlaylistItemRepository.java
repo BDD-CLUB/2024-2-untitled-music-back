6b014bf078f981ef04c2dede56bc0fcd636257ed
lcqff <yeonnu82g@gmail.com>
2025-06-20 21:25:34 +0900

    refactor: Playlist에 대한 추가 쿼리 제거
    #102

--- src/main/java/MusicPlatform/domain/playlist/_item/repository/PlaylistItemRepository.java
+++ src/main/java/MusicPlatform/domain/playlist/_item/repository/PlaylistItemRepository.java
@@ -6,8 +6,14 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlaylistItemRepository extends JpaRepository<PlaylistItem,Long>  {
+    @Query("SELECT p FROM PlaylistItem p "
+            + "JOIN FETCH p.track t "
+            + "JOIN FETCH t.album a "
+            + "JOIN FETCH a.artist at "
+            + "WHERE p.playlist = :playlist")
     Page<PlaylistItem> findAllByPlaylist(Playlist playlist, Pageable pageable);
     void deleteByUuid(String uuid);
 }
--- src/main/java/MusicPlatform/domain/playlist/repository/PlaylistRepository.java
+++ src/main/java/MusicPlatform/domain/playlist/repository/PlaylistRepository.java
@@ -1,13 +1,20 @@
 package MusicPlatform.domain.playlist.repository;
 
-import MusicPlatform.domain.artist.entity.Artist;
 import MusicPlatform.domain.playlist.entity.Playlist;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
 
+    @Query("SELECT p FROM Playlist p "
+            + "JOIN FETCH p.artist at "
+            + "WHERE p.uuid = :uuid")
     Optional<Playlist> findByUuid(String uuid);
-    List<Playlist> findAllByArtist(Artist artist);
+
+    @Query("SELECT p FROM Playlist p "
+            + "JOIN FETCH p.artist at "
+            + "WHERE at.uuid = :artistUuid")
+    List<Playlist> findAllByArtistUuid(String artistUuid);
 }
