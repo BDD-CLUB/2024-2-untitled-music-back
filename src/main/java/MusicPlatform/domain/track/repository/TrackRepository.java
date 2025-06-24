b425808a5ff18a59b122b276a20e79402ea07d39
lcqff <yeonnu82g@gmail.com>
2025-06-24 18:28:56 +0900

    refactor: Left Join 변경 - 옵티마이저 조회 순서 변경 #121

--- src/main/java/MusicPlatform/domain/track/repository/TrackRepository.java
+++ src/main/java/MusicPlatform/domain/track/repository/TrackRepository.java
@@ -10,19 +10,19 @@ import org.springframework.data.jpa.repository.Query;
 public interface TrackRepository extends JpaRepository<Track, Long> {
 
     @Query("SELECT t FROM Track t "
-            + "JOIN FETCH t.album a "
-            + "JOIN FETCH a.artist at "
+            + "LEFT JOIN FETCH t.album a "
+            + "LEFT JOIN FETCH a.artist at "
             + "WHERE t.uuid = :uuid")
     Optional<Track> findByUuid(String uuid);
 
     @Query("SELECT t FROM Track t "
-            + "JOIN FETCH t.album a "
-            + "JOIN FETCH a.artist at "
+            + "LEFT JOIN FETCH t.album a "
+            + "LEFT JOIN FETCH a.artist at "
             + "WHERE a.uuid = :artistUuid")
     Slice<Track> findAllByArtist(String artistUuid, Pageable pageable);
 
     @Query("SELECT t FROM Track t "
-            + "JOIN FETCH t.album a "
-            + "JOIN FETCH a.artist at ")
+            + "LEFT JOIN FETCH t.album a "
+            + "LEFT JOIN FETCH a.artist at ")
     Slice<Track> findAllBySlice(Pageable pageable);
 }
